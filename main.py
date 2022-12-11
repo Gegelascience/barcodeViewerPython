80931b0bad2b22901ce061f7689cdcf7179f1785
VERCHERE Rémi <remi.verchere2@gmail.com>
2022-12-11 11:43:43 +0100

    test ean13

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from EanGenerator import Ean8Generator, Ean13Generator
 # ean 13
 testBareCode13 = Ean13Generator("3666154117284")
 
-print("ean8")
+testBareCode13.saveBarcodeAsSvg("./test13.svg")
+
 # ean 8
 testBareCode8 = Ean8Generator("12345670")
 
+testBareCode8.saveBarcodeAsSvg("./test8.svg")
 
 testBareCode8.showBarcode()
-testBareCode8.saveBarcodeAsSvg("./test.svg")
