65a3a1a009a6ad0ebf2fac112097581ab03d5589
VERCHERE Rémi <remi.verchere2@gmail.com>
2023-01-14 17:19:31 +0100

    ajout de tests unitaires

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from EanGenerator import Ean8Generator, Ean13Generator
 
 
-# ean 13
-testBareCode13 = Ean13Generator("3666154117284")
 
-testBareCode13.saveBarcodeAsSvg("./test13.svg")
+if __name__ == '__main__':
+    
+    # ean 13
+    testBareCode13 = Ean13Generator("3666154117284")
+    testBareCode13.saveBarcodeAsSvg("./test13.svg")
 
-# ean 8
-testBareCode8 = Ean8Generator("12345670")
-
-testBareCode8.saveBarcodeAsSvg("./test8.svg")
-
-testBareCode8.showBarcode()
+    # ean 8
+    testBareCode8 = Ean8Generator("12345670")
+    testBareCode8.saveBarcodeAsSvg("./test8.svg")
+    testBareCode8.showBarcode()
---
+++ testCheckEan.py
@@ -0,0 +1,31 @@
+import unittest
+from EanCheckHelper import isCorrectEan,calculateDigitCheck, EanType
+from EanException import InvalidEanCharacter, InvalidCheckDigit
+
+class TestEanCheckHelper(unittest.TestCase):
+
+    def test_isCorrectEan_noerror(self):
+        self.assertEqual(isCorrectEan("3666154117284"),True)
+        self.assertEqual(isCorrectEan("3666154117284", EanType.EAN13),True)
+        
+
+    def test_isCorrectEan_error(self):
+        self.assertEqual(isCorrectEan("3666154117284", EanType.EAN8),False)
+        with self.assertRaises(InvalidCheckDigit) as InvCDE:
+            isCorrectEan("3666154117285")
+            self.assertEqual(InvCDE.exception.wrongDigit,"5")
+        with self.assertRaises(InvalidEanCharacter) as InvECE:
+            isCorrectEan("36661541172n4")
+ 
+        self.assertEqual(isCorrectEan(None),False)
+
+    def test_calculDigitCheck_noerror(self):
+        self.assertEqual(calculateDigitCheck("366615411728"),"4")
+
+    def test_calculDigitCheck_error(self):
+        self.assertRaises(TypeError,calculateDigitCheck, None)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
