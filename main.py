46bc93ec0ab945d8e5f22622ffacca2890dfe869
VERCHERE Rémi <remi.verchere2@gmail.com>
2023-01-22 15:45:24 +0100

    ajout ligne de commande

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from EanGenerator import Ean8Generator, Ean13Generator
+import sys
 
 
 
 if __name__ == '__main__':
-    
-    # ean 13
-    testBareCode13 = Ean13Generator("3666154117284")
-    testBareCode13.saveBarcodeAsSvg("./test13.svg")
 
-    # ean 8
-    testBareCode8 = Ean8Generator("12345670")
-    testBareCode8.saveBarcodeAsSvg("./test8.svg")
-    testBareCode8.showBarcode()
+    params = sys.argv
+
+    if len(params) > 1:
+        
+        possibleEan = params[1]
+
+        try:
+
+            if len(possibleEan) == 13:
+                # ean 13
+                testBareCode13 = Ean13Generator("3666154117284")
+                testBareCode13.saveBarcodeAsSvg("./test13.svg")
+                testBareCode13.showBarcode()
+
+            else:
+
+                # ean 8
+                testBareCode8 = Ean8Generator("12345670")
+                testBareCode8.saveBarcodeAsSvg("./test8.svg")
+                testBareCode8.showBarcode()
+
+        except:
+            sys.exit(1)
