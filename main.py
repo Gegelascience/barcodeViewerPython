d319d5c30eb6344b9259f27108281f4708b2b7b4
VERCHERE Rémi <remi.verchere2@gmail.com>
2023-09-23 15:29:02 +0200

    cleaner arg parser

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 from EanGenerator import Ean8Generator, Ean13Generator
 import sys
 from MainView import InputUI
+import argparse
 
+cmdArgParser =  argparse.ArgumentParser(description="show ean barcode")
+cmdArgParser.add_argument("-e","--ean", help="ean to show as barcode", type=str)
 
 if __name__ == '__main__':
 
-    params = sys.argv
+    params = cmdArgParser.parse_args()
 
-    if len(params) > 1:
+    if params.ean:
         
-        possibleEan = params[1]
 
         try:
 
-            if len(possibleEan) == 13:
+            if len(params.ean) == 13:
                 # ean 13
                 # valeur de test: 3666154117284
-                testBareCode13 = Ean13Generator(possibleEan)
+                testBareCode13 = Ean13Generator(params.ean)
                 testBareCode13.saveBarcodeAsSvg("./test13.svg")
                 testBareCode13.saveBarcodeAsPng("./test13.png")
                 testBareCode13.showBarcode()
                 
 
-            elif len(possibleEan) == 8:
+            elif len(params.ean) == 8:
 
                 # ean 8
                 # valeur de test: 12345670
-                testBareCode8 = Ean8Generator(possibleEan)
+                testBareCode8 = Ean8Generator(params.ean)
                 testBareCode8.saveBarcodeAsSvg("./test8.svg")
                 testBareCode8.saveBarcodeAsPng("./test8.png")
                 testBareCode8.showBarcode()
 
             else:
-                raise("Invalid EAN size")
+                raise Exception("Invalid EAN size")
 
         except Exception as e:
             print(e)
